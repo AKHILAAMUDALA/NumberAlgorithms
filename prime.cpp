522cb7ad562177e20cf054023397897a1e1c81be
AKHILAAMUDALA <69570857+AKHILAAMUDALA@users.noreply.github.com>
2021-05-12 10:52:17 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,c=0;
+	cin>>n;
+	for(int i=2;i<=n/2+1;i++)
+	{
+		if(n%i==0)
+		{
+			c++;
+			cout<<"not prime";
+			break;
+		}
+		
+	}
+	if(c==0)
+	{
+		cout<<"Prime";
+	}
+}
+
