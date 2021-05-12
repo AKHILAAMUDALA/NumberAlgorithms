553f68ce70b4d48e0ae93a8c9e1db5678579987c
AKHILAAMUDALA <69570857+AKHILAAMUDALA@users.noreply.github.com>
2021-05-12 17:40:56 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	int n,c=0;
 	cin>>n;
-	for(int i=2;i<=n/2+1;i++)
+	for(int i=2;i<=n/2+1;i++)//for(i=2;i<=n-1;i++)orfor(i=2;i<=sqrt(n+1);i++)
 	{
 		if(n%i==0)
 		{
@@ -19,5 +19,6 @@ int main()
 	{
 		cout<<"Prime";
 	}
+	return 0;
 }
 
